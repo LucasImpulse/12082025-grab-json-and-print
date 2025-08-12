ce7778c19731b23d7f079f741579698ce2c446c9
LucasImpulse <93647209+LucasImpulse@users.noreply.github.com>
2025-08-12 11:27:26 +0100

    comments plan it out

---
+++ task1.py
@@ -0,0 +1,16 @@
+# imports below this line
+
+
+# task says: 1. access https://my-json-server.typicode.com/marcuzh/router_location_test_api/db
+# i see a JSON format if i access using Chrome.
+
+
+# 2. read data from API
+# could just use a standard library here
+
+# 3. output list of connections between locations
+
+# so print iterate over a list
+# list has "connections" which describe a relationship between two nodes ("locations")
+# could just make a Connection class to cover that, which returns both location names
+# and allows handling duplicates easier.
